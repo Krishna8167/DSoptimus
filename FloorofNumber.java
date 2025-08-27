c7f999fbf466d586600aebaf1a9f153dddf34e9b
Krishna Sharma <156648921+Krishna8167@users.noreply.github.com>
2025-08-27 12:34:32 +0530

    Create FloorofNumber.java
    this problem solves the problem of floor of a number .
    Binary Search extensively used

---
+++ FloorofNumber.java
@@ -0,0 +1,26 @@
+
+class Main {
+    public static void main(String[] args) {
+       int[] arr = {1, 3, 5 ,7 , 16 , 34};
+       int target = 13;
+       int ans = ceiling(arr, target);
+       System.out.println("The index is : "+ans);
+    }
+    
+    static int ceiling(int[] arr ,int target) {
+        int s = 0 , e = arr.length-1;  // start-> s, end -> e. 
+        while(s <= e) {
+            int mid = s + (e-s)/2;
+            if(target < arr[mid]) {
+                e = mid -1;
+            }
+            else if(target > arr[mid]) {
+                s = mid+1;
+            }
+            else {
+               return mid;
+            }
+        }
+        return e;
+    }
+}

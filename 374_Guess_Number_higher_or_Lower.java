30a8811f3af2cd50ac3fc1294ed010286b289ded
Krishna Sharma <156648921+Krishna8167@users.noreply.github.com>
2025-12-06 11:18:40 +0530

    Update 374_Guess_Number_higher_or_Lower.java
    More solutions to the problem
    Count : 2

--- 374_Guess_Number_higher_or_Lower.java
+++ 374_Guess_Number_higher_or_Lower.java
@@ -1,3 +1,4 @@
+Sol: using regular binary Search.
 /** 
  * Forward declaration of guess API.
  * @param  num   your guess
@@ -23,3 +24,36 @@ public class Solution extends GuessGame {
         return -1; 
     }
 }
+
+Sol: Using recursion.
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        return search(1, n);
+    }
+
+    public int search(int s, int e) {
+        int mid = s + (e - s) / 2;
+
+        int result = guess(mid);
+
+        if (result == 0) {
+            return mid;
+        }
+
+        else if (result == -1) {
+            return search(s, mid - 1);
+        }
+
+        return search(mid + 1, e);
+    }
+}
+

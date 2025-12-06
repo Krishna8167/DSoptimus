701fa4c9db36c29c1d56a35983f5182d6c74c0b9
Krishna Sharma <156648921+Krishna8167@users.noreply.github.com>
2025-12-06 11:42:24 +0530

    Update 35_Search_insert_position.java
    Extended solution for the same problem

--- 35_Search_insert_position.java
+++ 35_Search_insert_position.java
@@ -1,3 +1,5 @@
+Solution 1 : using regular binary search
+
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int start = 0;
@@ -18,5 +20,36 @@ class Solution {
             }
         }
         return start; 
+    }
+}
+
+
+Solution 2: using recursion.
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        return search(start, end , nums, target);
+    }
+
+    public int search(int start, int end , int[]nums, int target) {
+        if (start > end) {
+            return start;
+        }
+        int mid = start + (end - start) / 2;
+
+        if (target == nums[mid]) {
+            return mid;
+        }
+
+        else if (target > nums[mid]) {
+            return search(mid + 1, end, nums, target);
+        }
+
+        else {
+            return search(start, mid - 1, nums, target);
+        }
     }
 }

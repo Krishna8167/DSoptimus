ba9aeaeac3f9123306bf39d672db1fdd7fcdc70c
Krishna Sharma <156648921+Krishna8167@users.noreply.github.com>
2025-12-12 15:13:43 +0530

    Update 441_Arranging_coins.java
    extended the possiblity of more solutions.

--- 441_Arranging_coins.java
+++ 441_Arranging_coins.java
@@ -5,3 +5,23 @@ class Solution {
         return (int) res;
     }
 }
+
+// with BInary Search approach
+class Solution {
+    public int arrangeCoins(int n) {
+        long start = 1, end = n;
+
+        while (start <= end) {
+            long mid = start + (end - start) / 2;
+            long coins = mid * (mid + 1) / 2;
+
+            if (coins == n)
+                return (int) mid;
+            if (coins < n)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return (int) end; // end is the largest valid k 
+    }
+}

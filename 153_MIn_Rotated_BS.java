c8b99d1d3917a8a82b81d51bc2a1495f8fc9bf3b
Krishna Sharma <156648921+Krishna8167@users.noreply.github.com>
2026-01-17 13:08:13 +0530

    Update 153_MIn_Rotated_BS.java
    2nd best optimal solution for the leetcode problem here.

--- 153_MIn_Rotated_BS.java
+++ 153_MIn_Rotated_BS.java
@@ -24,3 +24,24 @@ class Solution {
 
     }
 }
+
+
+#2. Solution OPtimal
+
+    class Solution {
+    public int findMin(int[] nums) {
+        return minSearch(nums, 0, nums.length - 1);
+    }
+
+    int minSearch(int[] nums, int s, int e) {
+        if (s == e) return nums[s];
+
+        int mid = s + (e - s) / 2;
+
+        if (nums[mid] > nums[e]) {
+            return minSearch(nums, mid + 1, e);
+        } else {
+            return minSearch(nums, s, mid);
+        }
+    }
+}

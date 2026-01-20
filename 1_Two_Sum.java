7918e6048648bfae5a90c892087feb976e7d9c91
Krishna Sharma <156648921+Krishna8167@users.noreply.github.com>
2026-01-20 13:14:29 +0530

    Create 1_Two_Sum.java
    1. Two Sum
    (Solved Easy)
    
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.
    
    Example 1:
    Input: nums = [2,7,11,15], target = 9
    Output: [0,1]
    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
    
    Example 2:
    Input: nums = [3,2,4], target = 6
    Output: [1,2]
    
    Example 3:
    Input: nums = [3,3], target = 6
    Output: [0,1]

---
+++ 1_Two_Sum.java
@@ -0,0 +1,16 @@
+case 1 : Brute force not the optimal solution.
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1}; // will not happen per problem statement
+    }
+}
+
+case 2: HashMap for the optimal solution.

27f56887691ca6f23a34df94e1b1924e056e92df
Krishna Sharma <156648921+Krishna8167@users.noreply.github.com>
2026-02-06 10:29:51 +0530

    Update 142_Linked_List_Cycle_ii.java

--- 142_Linked_List_Cycle_ii.java
+++ 142_Linked_List_Cycle_ii.java
@@ -57,3 +57,46 @@ public class Solution {
         return count;
     }
 }
+
+
+
+
+
+
+
+// Solution 2: Cyclic detection floyds algorithm / tortoise and hare algorithm.
+
+    /**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                fast = head;
+
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+    return null;
+    }
+}
